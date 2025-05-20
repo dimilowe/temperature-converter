a46f410e811b8ae8d2f59bd4e8dc3ffc6df37c74
Dimitrios Lowe <dimitrioslowe@gmail.com>
2025-05-20 10:03:07 -0400

    fix syntax

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 function convertToCelsius(fahrenheit) {
   const celsius = (fahrenheit - 32) * 0.55555555;
-  console.log(fahrenheit);
+  //console.log(fahrenheit);
   return celsius;
 }
 
-console.log(convertToCelsius(50));
+//console.log(convertToCelsius(50));
 
-function describeTemperature(fahrenheit) {
-  if (fahrenheit < 0) {
-    console.log(`very cold`);
-  } else if (fahrenheit < 20) {
-    console.log(`cold`);
-  } else if (fahrenheit < 30) {
-    console.log(`warm`);
-  } else if (fahrenheit < 40) {
-    console.log(`hot`);
-  } else fahrenheit >= 40;
-  console.log(`very hot`);
+function describeTemperature(celsius) {
+  if (celsius < 0) {
+    return `very cold`;
+  } else if (celsius < 20) {
+    return `cold`;
+  } else if (celsius < 30) {
+    return `warm`;
+  } else if (celsius < 40) {
+    return `hot`;
+  } else {
+    return `very hot`;
+  }
 }
-console.log(describeTemperature(55));
+//console.log(describeTemperature(0));
 
 const favoriteTemperatureInput = prompt(`Enter your ideal temperature`);
 const favoriteTemperature = Number(favoriteTemperatureInput);
 
-console.log(convertToCelsius(50), describeTemperature(55));
+//console.log(convertToCelsius(50), describeTemperature(55));
+
+alert(
+  `${favoriteTemperature} degrees fahrenheit is equal to ${convertToCelsius(
+    favoriteTemperature
+  )} degrees celsius.  That feels ${describeTemperature(
+    convertToCelsius(favoriteTemperature)
+  )}.`
+);
